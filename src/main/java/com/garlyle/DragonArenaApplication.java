0e2176d52ba7e46a8cef089017a10f0b37f2acc4
Garlyle <kovacsmilan85@gmail.com>
2017-09-08 18:41:26 +0200

    initial commit

---
+++ src/main/java/com/garlyle/DragonArenaApplication.java
@@ -0,0 +1,12 @@
+package com.garlyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonArenaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/garlyle/DragonArenaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.garlyle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DragonArenaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
